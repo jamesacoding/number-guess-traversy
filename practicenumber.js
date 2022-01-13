7ec31cf0991553cf315601786130a7bff381e68a
james a <jamesacoding@gmail.com>
2022-01-12 23:07:26 -0500

    Practice

--- practicenumber.js
+++ practicenumber.js
@@ -9,48 +9,44 @@
 
 let min = 1,
     max = 10,
-    winnningNum = 2,
-    guessesLeft = 3;
+    guessesLeft = 3,
+    winningNumb = 2;
 
 
-const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
+const minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
-      guessInput = document.querySelector('#guess-input'),
       guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input')
       message = document.querySelector('.message');
 
-      minNum.textContent = min;
-      maxNum.textContent = max;
+      console.log(message)
 
+minNum.textContent = min;
+maxNum.textContent = max;
 
 guessBtn.addEventListener('click', function(){
-    let guess = parseInt(guessInput.value)
-   if(isNaN(guess) || guess < min || guess > max ) {
-       setMessage(`Please pick a number between ${min} and ${max}`, 'red')
-   } 
-   if(guess === winnningNum){
-       guessInput.disabled = true;
-       guessInput.style.borderColor = 'green'
-       setMessage(`${winnningNum} is the correct answer`, 'green')
-   }else {
-       guessesLeft -= 1;
-       if(guessesLeft === 0){
-           guessInput.disabled = true;
-           guessInput.style.borderColor = 'red';
-           setMessage(`Game Over. You have no guesses left. The correct number was ${winnningNum}`, 'red')
-       }else {
-        setMessage(`${guess} is not correct. ${guessesLeft} Guesses Left`, 'red');
-        guessInput.value = '';
-       }
+    let guess = parseInt(guessInput.value);
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please pick a number between ${min} and ${max}`, 'red');
+    }
+    if(guess === winningNumb){
+        setMessage(`YOU WIN ${winningNumb} is the correct number`, 'green')
+    }else {
+        guessesLeft -= 1;
+        if(guessesLeft === 0) {
+            guessInput.disabled = true;
+            guessInput.value = '';
+            
+            setMessage(`Game Over`)
+        } else{
+        setMessage(`${guess} is not the number. You have ${guessesLeft} guesses left`, 'red');
+        guessInput.value = '';}
+    }
+});
 
-   }
 
-})
-
-function setMessage(msg, color) {
-    message.style.color = color;
+function setMessage(msg, color){
     message.textContent = msg;
+    message.style.color = color;
 }
-
 
